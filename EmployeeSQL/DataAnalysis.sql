c7bf05e7ae5dd966c86a4fc0a573b67e2a4aa5b9
nconnel4 <nconnel4@hotmail.com>
2021-05-12 19:50:04 -0400

    Add question 1 query

---
+++ EmployeeSQL/DataAnalysis.sql
@@ -0,0 +1,4 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employee e
+    JOIN salary s ON s.emp_no = e.emp_no;
