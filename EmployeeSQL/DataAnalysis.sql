6bba1872a934feabdd61e086465b1ce5a16c1f26
nconnel4 <nconnel4@hotmail.com>
2021-05-12 19:56:12 -0400

    Add query for hire date in 1986

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -2,3 +2,8 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employee e
     JOIN salary s ON s.emp_no = e.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employee
+WHERE DATE_PART('year', hire_date) = 1986
