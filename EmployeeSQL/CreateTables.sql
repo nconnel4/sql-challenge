f572964ebebd0fbe96a63c2c5d2bb328d551a265
nconnel4 <nconnel4@hotmail.com>
2021-05-12 19:20:47 -0400

    Add foreign keys

--- EmployeeSQL/CreateTables.sql
+++ EmployeeSQL/CreateTables.sql
@@ -15,22 +15,40 @@ create table employee(
     first_name varchar(25),
     last_name varchar(25),
     sex char(1),
-    hire_date date
+    hire_date date,
+    constraint fk_title
+        foreign key(emp_title_id)
+            references title(title_id)
 );
 
 create table salary(
     emp_no int,
-    salary numeric(10, 2)
+    salary numeric(10, 2),
+    constraint fk_employee
+        foreign key(emp_no)
+            references employee(emp_no)
 );
 
 create table dept_manager(
     dept_no char(4),
-    emp_no int
+    emp_no int,
+    constraint fk_department
+        foreign key(dept_no)
+                         references department(dept_no),
+    constraint fk_employee
+                         foreign key (emp_no)
+                         references employee(emp_no)
 );
 
 create table dept_emp(
     emp_no int,
-    dept_no char(4)
+    dept_no char(4),
+    constraint fk_department
+        foreign key(dept_no)
+                         references department(dept_no),
+    constraint fk_employee
+                         foreign key (emp_no)
+                         references employee(emp_no)
 );
 
 
