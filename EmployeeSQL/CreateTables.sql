ad0bff9bc74cb35d5fdd2b8534e5f4bd66dc7e41
nconnel4 <nconnel4@hotmail.com>
2021-05-12 19:21:05 -0400

    Fix file formatting

--- EmployeeSQL/CreateTables.sql
+++ EmployeeSQL/CreateTables.sql
@@ -1,54 +1,60 @@
-create table title(
+create table title
+(
     title_id char(5) primary key,
-    title varchar(25)
+    title    varchar(25)
 );
 
-create table department(
-    dept_no char(4) primary key,
+create table department
+(
+    dept_no   char(4) primary key,
     dept_name varchar(25)
 );
 
-create table employee(
-    emp_no int primary key,
+create table employee
+(
+    emp_no       int primary key,
     emp_title_id char(5),
-    birth_date date,
-    first_name varchar(25),
-    last_name varchar(25),
-    sex char(1),
-    hire_date date,
+    birth_date   date,
+    first_name   varchar(25),
+    last_name    varchar(25),
+    sex          char(1),
+    hire_date    date,
     constraint fk_title
-        foreign key(emp_title_id)
-            references title(title_id)
+        foreign key (emp_title_id)
+            references title (title_id)
 );
 
-create table salary(
+create table salary
+(
     emp_no int,
     salary numeric(10, 2),
     constraint fk_employee
-        foreign key(emp_no)
-            references employee(emp_no)
+        foreign key (emp_no)
+            references employee (emp_no)
 );
 
-create table dept_manager(
+create table dept_manager
+(
     dept_no char(4),
-    emp_no int,
+    emp_no  int,
     constraint fk_department
-        foreign key(dept_no)
-                         references department(dept_no),
+        foreign key (dept_no)
+            references department (dept_no),
     constraint fk_employee
-                         foreign key (emp_no)
-                         references employee(emp_no)
+        foreign key (emp_no)
+            references employee (emp_no)
 );
 
-create table dept_emp(
-    emp_no int,
+create table dept_emp
+(
+    emp_no  int,
     dept_no char(4),
     constraint fk_department
-        foreign key(dept_no)
-                         references department(dept_no),
+        foreign key (dept_no)
+            references department (dept_no),
     constraint fk_employee
-                         foreign key (emp_no)
-                         references employee(emp_no)
+        foreign key (emp_no)
+            references employee (emp_no)
 );
 
 
